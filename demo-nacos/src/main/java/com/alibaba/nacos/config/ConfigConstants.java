e163fde61810af0c27004fdddea7115c722a9498
dongp <dongp@laschain.com>
2022-07-01 16:30:41 +0800

    集成nacos

---
+++ demo-nacos/src/main/java/com/alibaba/nacos/config/ConfigConstants.java
@@ -0,0 +1,29 @@
+package com.alibaba.nacos.config;
+
+/**
+ * @Description 覆盖nacos 默认配置
+ * @Author dongp
+ * @Date 2022/7/1 0001 16:22
+ */
+public interface ConfigConstants {
+
+    /**
+     * The System property name of Standalone mode
+     */
+    String STANDALONE_MODE = "nacos.standalone";
+
+    /**
+     * 是否开启认证
+     */
+    String AUTH_ENABLED = "nacos.core.auth.enabled";
+
+    /**
+     * 日志目录
+     */
+    String LOG_BASEDIR = "server.tomcat.basedir";
+
+    /**
+     * access_log日志开关
+     */
+    String LOG_ENABLED = "server.tomcat.accesslog.enabled";
+}
