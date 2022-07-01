269d28fad17f1e8221d7a4a469417f24bd2b8fcb
dongp <dongp@laschain.com>
2022-07-01 17:29:20 +0800

    gateway集成springDoc UI

--- demo-gateway/src/main/java/com/roc/demo/gateway/DemoGatewayApplication.java
+++ demo-gateway/src/main/java/com/roc/demo/gateway/DemoGatewayApplication.java
@@ -2,12 +2,14 @@ package com.roc.demo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Description 网关应用
  * @Author dongp
  * @Date 2022/7/1 0001 16:32
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoGatewayApplication {
     public static void main(String[] args) {
